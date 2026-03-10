b1737601ab09300269f709e9cecc9a13a80f25e2
pradhappn <pradhappns222@gmail.com>
2026-03-10 11:03:27 +0530

    feat: restore lost backend API files

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['customer' , 'admin'],
+        default: 'customer'
+    },
+    address: { street: String, city: String, state: String, zip: String},
+    phone: {type: String},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}, 
+    orderHistory: [{type: mongoose.Schema.Types.ObjectId, ref: 'Order'}]
+}, { timestamps: true});
+
+module.exports = mongoose.model('User', userSchema);
